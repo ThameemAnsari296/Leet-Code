c02845e5d0970f5178fda0b5beb4934af0a0063a
Thameem Ansari <ansarithameem296@gmail.com>
2025-10-24 09:37:33 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (81.82%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,8 +1,17 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String words[] =s.split(" ");
-        String last= words[words.length-1];
-        int length=last.length();
+        s = s.trim();
+        
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            }
+            else if (length > 0) {
+                break;
+            }
+        }
+        
         return length;
     }
 }
